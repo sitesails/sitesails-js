7d7e26db251f96fb9a215e8658c088cd577c32a8
Tomislav Petrovic <tomislav@initgrupa.hr>
2020-12-29 08:57:28 +0100

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import SiteSailsClient from './client';
 
+export { Configuration } from './client/types';
+export * from './collections/types';
+export * from './connections/types';
+export * from './members/types';
+export * from './nodes/types';
+export * from './types';
+
 export default SiteSailsClient;
