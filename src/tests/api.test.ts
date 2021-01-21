e39bab533c53ad68261071854f08edd1585429aa
Karlo Marinović <marinovic.karlo@gmail.com>
2021-01-21 16:00:37 +0100

    replaced wrapper functions with direct calls to ss module

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -1,50 +1,53 @@
 import {
-  fetchNodeCategories,
-  fetchNodes,
-  getNode,
-  getNodeCategory,
+  // addConnection,
+  // fetchNodeCategories,
+  // fetchNodes,
+  // getNode,
   loginMember,
-  // registerMember,
+  ss,
 } from './setup';
 
-describe('Api nodes', () => {
-  it('should all be fetched correctly', async () => {
-    const { data } = await fetchNodes();
+const userData = {
+  email: 'karlo.marinovic@init.hr',
+  firstName: 'Karlo',
+  lastName: 'Marinović',
+  password: 'matrixmatrix',
+};
 
+describe('Api nodes', () => {
+  const nodes = 'products';
+
+  it('should all be fetched correctly', async () => {
+    const nodes = 'products';
+    const { data } = await ss.nodes(nodes).search({});
     expect(data).toHaveLength(4);
   });
 
   it('should be fetched only from a specified node category', async () => {
     const category = 'sport';
-    const { data } = await fetchNodes({ category });
-
+    const { data } = await ss.nodes(nodes).search({ category });
     expect(data).toHaveLength(2);
   });
   it('should fetch only in specified language', async () => {
     const lang = 'hr';
-    const { data } = await fetchNodes({ lang });
-
+    const { data } = await ss.nodes(nodes).search({ lang });
     expect(data[0].languageId).toEqual(lang);
   });
 
   it('should fetch a specified node', async () => {
     const slug = 'football';
-
-    const { data } = await getNode(slug);
-
+    const { data } = await ss.nodes(nodes).get(slug);
     expect(data.title).toEqual('Football');
   });
 
   it('should fetch a specified node in specified language', async () => {
     const slug = 'football';
     const lang = 'hr';
-
-    const { data } = await getNode(slug, { lang });
-
+    const { data } = await ss.nodes(nodes).get(slug, { lang });
     expect(data.title).toEqual('Nogometna lopta');
   });
 
-  it('should be of specific shape when fetched', async () => {
+  it('should be of specific shape when a node is fetched', async () => {
     const slug = 'football';
     const expectedProduct = {
       id: 2856,
@@ -77,46 +80,35 @@ describe('Api nodes', () => {
       subsections: null,
     };
 
-    const node = await getNode(slug);
-
+    const node = await ss.nodes(nodes).get(slug);
     expect(node).toEqual(expectedProduct);
   });
 
   it('categories should all be fetched correctly', async () => {
-    const section = 'products';
-    const { data } = await fetchNodeCategories(section);
-
+    const { data } = await ss.nodes(nodes).categories({});
     expect(data).toHaveLength(2);
   });
 
   it('categories should all be fetched correctly in specified language', async () => {
-    const section = 'products';
     const lang = 'hr';
-    const { data } = await fetchNodeCategories(section, { lang });
-
+    const { data } = await ss.nodes(nodes).categories({ lang });
     expect(data[0].languageId).toEqual(lang);
   });
 
   it('category should be fetched correctly', async () => {
-    const section = 'products';
     const category = 'food';
-
-    const { data } = await getNodeCategory(section, category);
-
+    const { data } = await ss.nodes(nodes).category(category);
     expect(data.name).toEqual(category);
   });
 
   it('category should be fetched in specified language', async () => {
-    const section = 'products';
     const category = 'food';
     const lang = 'hr';
-
-    const { data } = await getNodeCategory(section, category, { lang });
+    const { data } = await ss.nodes(nodes).category(category, { lang });
     expect(data.name).toEqual('hrana');
   });
 
   it('category should be fetched in specific shape', async () => {
-    const section = 'products';
     const category = 'food';
     const expectedResponse = {
       id: 2855,
@@ -148,39 +140,32 @@ describe('Api nodes', () => {
       subsections: null,
     };
 
-    const res = await getNodeCategory(section, category);
+    const res = await ss.nodes(nodes).category(category);
+
     expect(res).toEqual(expectedResponse);
   });
 });
 describe('Api members', () => {
-  const userData = {
-    email: 'karlo.marinovic@init.hr',
-    firstName: 'Karlo',
-    lastName: 'Marinović',
-    password: 'matrixmatrix',
-  };
   test('should register new user correctly', async () => {
+    // TODO ADD DYNAMIC EMAIL SO NO EMAIL CONFLICT
     // await registerMember(userData);
   });
 
   test('should login existing user corectly', async () => {
     const expectedLoginResponse = {
-      id: 9,
-      token: 'token',
-      email: 'karlo.marinovic@init.hr',
-      firstName: 'Karlo',
-      lastName: 'Marinović',
+      id: 0,
+      token: '',
+      email: '',
+      firstName: '',
+      lastName: '',
       avatarUrl: null,
       hasNotificationConsent: true,
     };
 
     const expectedKeys = Object.keys(expectedLoginResponse);
-
-    const res = await loginMember({
-      email: userData.email,
-      password: userData.password,
-    });
-
+    const res = await ss
+      .members()
+      .login({ email: userData.email, password: userData.password });
     const resKeys = Object.keys(res);
 
     expect(resKeys).toEqual(expectedKeys);
@@ -189,6 +174,16 @@ describe('Api members', () => {
 
 describe('Api connections', () => {
   // test that a node can be liked
+
+  it('adds a connection to a node correctly', async () => {
+    // const connection = 'likes';
+    // // const productId =
+    // const loggedUser = await loginMember({
+    //   email: userData.email,
+    //   password: userData.password,
+    // });
+    // const res = await addConnection(connection, loggedUser,  )
+  });
   // test taht a node can be unliked
   // test that a node has a like when fetched
   // test search for connections on a node?
