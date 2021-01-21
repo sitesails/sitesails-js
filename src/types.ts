1a14cb2071520cada0ae2e0fb85c1d4d1b0e6be7
Karlo Marinović <marinovic.karlo@gmail.com>
2021-01-21 12:03:53 +0100

    tested fetcing all products, category products, products in language, and categories

--- src/types.ts
+++ src/types.ts
@@ -26,6 +26,8 @@ export type Node = {
   connections: any;
   connectionStats: any;
   event: NodeEvent;
+  // TODO ADDED LANGUAGE ID? TO BE ABLE TO ACCESS LANGUAGE IN THE TESTS
+  languageId?: string;
 };
 
 export type ResponseMeta = {
