05660a78fdab090d541a09ef1cf8fea90dd3c81f
Karlo Marinović <marinovic.karlo@gmail.com>
2021-01-24 18:19:46 +0100

    added and tested set and delete methods

--- src/keyvalues/index.ts
+++ src/keyvalues/index.ts
@@ -1,4 +1,3 @@
-// TODO
 import SiteSailsClient from '../client';
 import {
   KeyValue,
@@ -6,8 +5,6 @@ import {
   KeyValueSearchParams,
   KeyValueSearchResult,
 } from './types';
-
-// TODO CREATE CLASS FOR KEY VALUE
 
 export default class SiteSailsKeyValueManager {
   client: SiteSailsClient;
@@ -17,31 +14,6 @@ export default class SiteSailsKeyValueManager {
     this.client = client;
     this.store = store;
   }
-  // TODO TEST FOW NOW // NEED RETURN TYPE TOO
-  async testAllStores() {
-    const res = await this.client.fetch(
-      // '/keyvalues/redirect/test',
-      // { value: 'three' },
-
-      '/keyvalues/redirect/',
-      {
-        // search: 'one',
-        pageSize: 1,
-        pageNumber: 3,
-      },
-      {
-        method: 'get',
-        // method: 'post',
-        headers: {
-          Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyOSIsImdyb3Vwc2lkIjoiMTkiLCJlbWFpbCI6ImUyZS10ZXN0LWpzLWFkbWluQHNpdGVzYWlscy5jb20iLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE2MTE0MDI0MDAsImV4cCI6MTY0MjkzODQwMCwiaWF0IjoxNjExNDAyNDAwfQ.GMHM-FfP6F8Ed0LlVeV8X6DYCzVe7FM0xV4Cm8KQQ7s'}`,
-        },
-        // body: {
-        //   value: 'less',
-        // },
-      },
-    );
-    return res;
-  }
 
   async search<T = KeyValue>(
     params?: KeyValueSearchParams,
@@ -49,11 +21,64 @@ export default class SiteSailsKeyValueManager {
   ): Promise<KeyValueSearchResult<T>> {
     const res = await this.client.fetch(`/keyvalues/${this.store}`, params, {
       transformation,
+      // TODO REMOVE HEADERS
       headers: {
         Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyOSIsImdyb3Vwc2lkIjoiMTkiLCJlbWFpbCI6ImUyZS10ZXN0LWpzLWFkbWluQHNpdGVzYWlscy5jb20iLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE2MTE0MDI0MDAsImV4cCI6MTY0MjkzODQwMCwiaWF0IjoxNjExNDAyNDAwfQ.GMHM-FfP6F8Ed0LlVeV8X6DYCzVe7FM0xV4Cm8KQQ7s'}`,
       },
       list: true,
     });
+
+    return res;
+  }
+
+  async get<T = KeyValue>(
+    key: string,
+    transformation?: KeyValueResponseTransformation<T>,
+  ): Promise<T> {
+    const res = await this.client.fetch(
+      `/keyvalues/${this.store}/${key}`,
+      null,
+      {
+        transformation,
+        // TODO REMOVE HEADERS
+        headers: {
+          Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyOSIsImdyb3Vwc2lkIjoiMTkiLCJlbWFpbCI6ImUyZS10ZXN0LWpzLWFkbWluQHNpdGVzYWlscy5jb20iLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE2MTE0MDI0MDAsImV4cCI6MTY0MjkzODQwMCwiaWF0IjoxNjExNDAyNDAwfQ.GMHM-FfP6F8Ed0LlVeV8X6DYCzVe7FM0xV4Cm8KQQ7s'}`,
+        },
+      },
+    );
+
+    return res;
+  }
+
+  async set(key: string, value: string): Promise<KeyValue> {
+    const res = await this.client.fetch(
+      `/keyvalues/${this.store}/${key}`,
+      null,
+      {
+        method: 'post',
+        // TODO REMOVE HEADERS
+        headers: {
+          Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyOSIsImdyb3Vwc2lkIjoiMTkiLCJlbWFpbCI6ImUyZS10ZXN0LWpzLWFkbWluQHNpdGVzYWlscy5jb20iLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE2MTE0MDI0MDAsImV4cCI6MTY0MjkzODQwMCwiaWF0IjoxNjExNDAyNDAwfQ.GMHM-FfP6F8Ed0LlVeV8X6DYCzVe7FM0xV4Cm8KQQ7s'}`,
+        },
+        body: { value },
+      },
+    );
+
+    return res;
+  }
+
+  async delete(key: string): Promise<KeyValue> {
+    const res = await this.client.fetch(
+      `/keyvalues/${this.store}/${key}`,
+      null,
+      {
+        method: 'delete',
+        // TODO REMOVE HEADERS
+        headers: {
+          Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyOSIsImdyb3Vwc2lkIjoiMTkiLCJlbWFpbCI6ImUyZS10ZXN0LWpzLWFkbWluQHNpdGVzYWlscy5jb20iLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE2MTE0MDI0MDAsImV4cCI6MTY0MjkzODQwMCwiaWF0IjoxNjExNDAyNDAwfQ.GMHM-FfP6F8Ed0LlVeV8X6DYCzVe7FM0xV4Cm8KQQ7s'}`,
+        },
+      },
+    );
 
     return res;
   }
--- src/keyvalues/keyvalues.test.ts
+++ src/keyvalues/keyvalues.test.ts
@@ -1,6 +1,20 @@
-import { toMatchShapeOf } from 'jest-to-match-shape-of';
+import { toMatchShapeOf, toMatchOneOf } from 'jest-to-match-shape-of';
+
 import SiteSailsClient from '../client';
-import { KeyValue, KeyValueSearchResult } from './types';
+import {
+  KeyValue,
+  KeyValueResponseTransformation,
+  KeyValueSearchResult,
+} from './types';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    interface Matchers<R> {
+      toIncludeSearchStringInKeyValue(expected: string): R;
+    }
+  }
+}
 
 const ss = new SiteSailsClient({
   apiUrl: 'https://api.sitesails.com/api/v1',
@@ -9,14 +23,47 @@ const ss = new SiteSailsClient({
   // debug: true,
 });
 
-const store = 'redirect';
+// TODO WILL NEED ADMIN KEY HERE INSTEAD OF USING TOKEN IN METHODS
 
-// TODO WILL NEED ADMIN KEY HERE
-// TODO BEFORE GET ADMIN KEY, USE UI ADMIN TOKEN IN EACH REQUEST
+const toIncludeSearchStringInKeyValue: jest.CustomMatcher = (
+  received: KeyValue,
+  expected: string,
+) => {
+  if (typeof expected !== 'string') {
+    throw new Error('Expected searchString to be a string!');
+  }
+
+  if (received.key.includes(expected) || received.value.includes(expected))
+    return {
+      pass: true,
+      message: () => `Search results expected to contain '${expected}'`,
+    };
+
+  return {
+    pass: false,
+    message: () => `Search results expected to contain '${expected}':
+    result: 
+    {
+      key: ${received.key}
+      value: ${received.value}
+    }
+    `,
+  };
+};
 
 expect.extend({
   toMatchShapeOf,
+  toMatchOneOf,
+  toIncludeSearchStringInKeyValue,
 });
+
+const store = 'redirect';
+type TransformedKeyValueTuple = [key: string, value: string];
+const transformFunction: KeyValueResponseTransformation<TransformedKeyValueTuple> = (
+  response,
+) => {
+  return [response.key, response.value];
+};
 
 describe('KeyValues search method', () => {
   it('should return all keyvalue pairs from the specified store', async () => {
@@ -41,26 +88,122 @@ describe('KeyValues search method', () => {
     expect(res).toMatchShapeOf(expectedResponseShape);
   });
 
-  // SEARCH RESULTS COME TRANSFORMED AS PER SPECIFIED TYPE
   it('should return response that matches transformed type when transform function is provided', async () => {
-    type TransformedKeyValueTuple = [key: string, value: string];
     const expectedTransformedShape: KeyValueSearchResult<TransformedKeyValueTuple> = {
       data: [['one', 'less']],
       meta: { pageNumber: 1, pageSize: 20, totalRows: 3, totalPages: 1 },
     };
-    const transformFunction = (
-      response: KeyValue,
-    ): TransformedKeyValueTuple => {
-      return [response.key, response.value];
-    };
 
-    const res = await ss.keyvalues(store).search(null, transformFunction);
+    const res = await ss
+      .keyvalues(store)
+      .search<TransformedKeyValueTuple>(null, transformFunction);
 
     expect(res).toMatchShapeOf(expectedTransformedShape);
   });
 
-  // need to specify return type on the method
-  // SEARCH PER PAGE
-  // SEARH PAGE NR
-  // SEARCH STRING SEARCH
+  it('should return specified number of results per page', async () => {
+    const expectedNumberOfResults = 1;
+
+    const { data } = await ss
+      .keyvalues(store)
+      .search({ pageSize: expectedNumberOfResults });
+
+    expect(data).toHaveLength(expectedNumberOfResults);
+  });
+
+  it('should return specified results page', async () => {
+    const expectedResultsPage = 2;
+    const numberOfResults = 1;
+
+    const { meta } = await ss
+      .keyvalues(store)
+      .search({ pageSize: numberOfResults, pageNumber: expectedResultsPage });
+
+    expect(meta.pageNumber).toBe(expectedResultsPage);
+  });
+
+  it('should return all entries where either key or value contains specified value string', async () => {
+    const expectedSearchString = 'one';
+
+    const { data } = await ss
+      .keyvalues(store)
+      .search({ search: expectedSearchString });
+
+    data.forEach((result) => {
+      expect(result).toIncludeSearchStringInKeyValue(expectedSearchString);
+    });
+  });
+});
+
+describe('KeyValue get method', () => {
+  const expectedKeyValueObject: KeyValue = {
+    key: 'another',
+    value: 'another-one',
+  };
+
+  it('should return correct value for a specified key', async () => {
+    const res = await ss.keyvalues(store).get(expectedKeyValueObject.key);
+
+    expect(res.value).toBe(expectedKeyValueObject.value);
+  });
+
+  it('should return an object of correct shape', async () => {
+    const res = await ss.keyvalues(store).get(expectedKeyValueObject.key);
+
+    expect(res).toMatchShapeOf(expectedKeyValueObject);
+  });
+
+  it('should return a correctly transformed response when transform function is provided ', async () => {
+    const expectedTransformedResponse: TransformedKeyValueTuple = [
+      'another',
+      'another-one',
+    ];
+
+    const res = await ss
+      .keyvalues(store)
+      .get(expectedKeyValueObject.key, transformFunction);
+
+    expect(res).toEqual(expectedTransformedResponse);
+  });
+});
+
+describe('KeyValue set method', () => {
+  it('should update value of an existing key', async () => {
+    const existingKey = 'one';
+    const newValue = 'more';
+
+    const res = await ss.keyvalues(store).set(existingKey, newValue);
+
+    expect(res.value).toBe(newValue);
+  });
+
+  it('should add a new keyvalue pair if provided key does not exist', async () => {
+    const newKeyValue: KeyValue = {
+      key: 'two',
+      value: 'more',
+    };
+
+    const res = await ss
+      .keyvalues(store)
+      .set(newKeyValue.key, newKeyValue.value);
+
+    expect(res).toMatchObject(newKeyValue);
+  });
+});
+
+describe('KeyValue delete method', () => {
+  it('should delete an existing keyvalue entry when called', async () => {
+    const deletedKeyValue: KeyValue = {
+      key: 'two',
+      value: 'more',
+    };
+
+    await ss.keyvalues(store).delete(deletedKeyValue.key);
+
+    const { data: allKeyValuesResponse } = await ss.keyvalues(store).search();
+
+    allKeyValuesResponse.forEach((keyValue) => {
+      expect(keyValue).not.toMatchObject(deletedKeyValue);
+    });
+  });
 });
