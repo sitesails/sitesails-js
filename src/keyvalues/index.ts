8f506e246890886a34795c8e082017702808830b
Tomislav Petrovic <tomislav@initgrupa.hr>
2021-01-25 08:57:48 +0100

    fix keyvalue test

--- src/keyvalues/index.ts
+++ src/keyvalues/index.ts
@@ -21,11 +21,8 @@ export default class SiteSailsKeyValueManager {
   ): Promise<KeyValueSearchResult<T>> {
     const res = await this.client.fetch(`/keyvalues/${this.store}`, params, {
       transformation,
-      // TODO REMOVE HEADERS
-      headers: {
-        Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyOSIsImdyb3Vwc2lkIjoiMTkiLCJlbWFpbCI6ImUyZS10ZXN0LWpzLWFkbWluQHNpdGVzYWlscy5jb20iLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE2MTE0MDI0MDAsImV4cCI6MTY0MjkzODQwMCwiaWF0IjoxNjExNDAyNDAwfQ.GMHM-FfP6F8Ed0LlVeV8X6DYCzVe7FM0xV4Cm8KQQ7s'}`,
-      },
       list: true,
+      admin: true,
     });
 
     return res;
@@ -40,10 +37,7 @@ export default class SiteSailsKeyValueManager {
       null,
       {
         transformation,
-        // TODO REMOVE HEADERS
-        headers: {
-          Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyOSIsImdyb3Vwc2lkIjoiMTkiLCJlbWFpbCI6ImUyZS10ZXN0LWpzLWFkbWluQHNpdGVzYWlscy5jb20iLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE2MTE0MDI0MDAsImV4cCI6MTY0MjkzODQwMCwiaWF0IjoxNjExNDAyNDAwfQ.GMHM-FfP6F8Ed0LlVeV8X6DYCzVe7FM0xV4Cm8KQQ7s'}`,
-        },
+        admin: true,
       },
     );
 
@@ -56,11 +50,8 @@ export default class SiteSailsKeyValueManager {
       null,
       {
         method: 'post',
-        // TODO REMOVE HEADERS
-        headers: {
-          Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyOSIsImdyb3Vwc2lkIjoiMTkiLCJlbWFpbCI6ImUyZS10ZXN0LWpzLWFkbWluQHNpdGVzYWlscy5jb20iLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE2MTE0MDI0MDAsImV4cCI6MTY0MjkzODQwMCwiaWF0IjoxNjExNDAyNDAwfQ.GMHM-FfP6F8Ed0LlVeV8X6DYCzVe7FM0xV4Cm8KQQ7s'}`,
-        },
         body: { value },
+        admin: true,
       },
     );
 
@@ -73,10 +64,7 @@ export default class SiteSailsKeyValueManager {
       null,
       {
         method: 'delete',
-        // TODO REMOVE HEADERS
-        headers: {
-          Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyOSIsImdyb3Vwc2lkIjoiMTkiLCJlbWFpbCI6ImUyZS10ZXN0LWpzLWFkbWluQHNpdGVzYWlscy5jb20iLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE2MTE0MDI0MDAsImV4cCI6MTY0MjkzODQwMCwiaWF0IjoxNjExNDAyNDAwfQ.GMHM-FfP6F8Ed0LlVeV8X6DYCzVe7FM0xV4Cm8KQQ7s'}`,
-        },
+        admin: true,
       },
     );
 
